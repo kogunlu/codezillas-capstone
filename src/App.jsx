9a3502fd3e34a2ceaad125333369c4c9954f384a
Kuday Ogunlu <kudayogunlu@gmail.com>
2022-12-22 11:40:37 +0300

    Setup | firebase add

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 
+
 function App() {
+
+
   return (
     <div className="App">
       <header className="App-header">

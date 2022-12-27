eee21e919ed2a2493d1c12dac1bfb0a8e626b270
Kuday Ogunlu <kudayogunlu@gmail.com>
2022-12-27 11:03:15 +0300

    Task complete. Test required

--- src/features/booking/bookingSlice.js
+++ src/features/booking/bookingSlice.js
@@ -18,33 +18,25 @@ export const bookingSlice = createSlice({
     reducers: {
         setAnswer1: (state, action) => {
             state.answers.answer1 = action.payload
-            console.log("answer 1 updated => ", state.answers.answer1)
         },
         setAnswer2: (state, action) => {
             state.answers.answer2 = action.payload
-            console.log("answer 2 updated =>", state.answers.answer2)
         },
         setAnswer3: (state, action) => {
             state.answers.answer3 = action.payload
-            console.log("answer 3 updated =>", state.answers.answer3)
         },
         setAnswer4: (state, action) => {
             state.answers.answer4 = action.payload
-            console.log("answer 4 update =>", state.answers.answer4)
         },
         setAnswer5: (state, action) => {
             state.answers.answer5 = action.payload
-            console.log("answer 5 update => ", state.answers.answer5)
         },
         setAnswer6: (state, action) => {
             state.answers.answer6 = action.payload
-            console.log("answer 6 update => ", state.answers.answer6)
         }
     }
 })
 
-  // auto close tag, auto rename, babel javascript, color highlet, dotENV, jest, code snippets js es6, npm intellisense, tailwind intellisense, 
-  
 export const {
     setAnswer1, 
     setAnswer2, 

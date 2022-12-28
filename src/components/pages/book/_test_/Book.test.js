98a08aa24a916806d24232886651258b5a3596de
Kuday Ogunlu <kudayogunlu@gmail.com>
2022-12-28 17:30:54 +0300

    Improvement | Book component's test is fixed. Issue complete

--- src/components/pages/book/_test_/Book.test.js
+++ src/components/pages/book/_test_/Book.test.js
@@ -2,17 +2,18 @@ import { Provider } from "react-redux"
 import { BrowserRouter } from "react-router-dom"
 import renderer from "react-test-renderer"
 import Book from "../Book"
-import store from "../../../../app/store"
+import {store} from "../../../../app/store"
 
 
-it('renders correctly', async() => {
-    // const bookComponent = renderer
-    .create(
-        <Provider store={store}>
+
+it(`renders correctly`, () => {
+    const bookComponent = renderer.create(
+        <Provider store={store} >
             <BrowserRouter>
                 <Book />
             </BrowserRouter>
         </Provider>
+        
     )
     .toJSON()
         expect(bookComponent).toMatchSnapshot()

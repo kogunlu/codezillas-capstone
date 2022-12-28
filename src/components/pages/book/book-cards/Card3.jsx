fee3575e55bffd929a195c5d975f09cbc79d2197
Kuday Ogunlu <kudayogunlu@gmail.com>
2022-12-28 16:31:38 +0300

    Refactor the Card3 component | typo fix

--- src/components/pages/book/book-cards/Card3.jsx
+++ src/components/pages/book/book-cards/Card3.jsx
@@ -39,14 +39,14 @@ function Card3({btnClick}) {
             <input 
             type="button" 
             value="Yes" 
-            className='w-full border border-solid border-2 border-gray-200 rounded-lg text-md md:text-xl font-semibold pl-2 py-2 text-left hover:bg-gray-100 focus:bg-cyan-400 focus:text-slate-100'
+            className='w-full border-solid border-2 border-gray-200 rounded-lg text-md md:text-xl font-semibold pl-2 py-2 text-left hover:bg-gray-100 focus:bg-cyan-400 focus:text-slate-100'
             onClick={e => setSelected(e.target.value)} 
             />
 
             <input 
             type="button" 
             value="No" 
-            className='w-full border border-solid border-2 border-gray-200 rounded-lg text-md md:text-xl font-semibold pl-2 py-2 text-left hover:bg-gray-100 focus:bg-cyan-400 focus:text-slate-100'
+            className='w-full border-solid border-2 border-gray-200 rounded-lg text-md md:text-xl font-semibold pl-2 py-2 text-left hover:bg-gray-100 focus:bg-cyan-400 focus:text-slate-100'
             onClick={e => setSelected(e.target.value)}  
             />
 

ab38dff674359a71f9d8055e1604a296f627ec0a
Kuday Ogunlu <kudayogunlu@gmail.com>
2023-01-04 12:58:41 +0300

    Style | Login page responsive complete

--- src/components/pages/Login/Login.jsx
+++ src/components/pages/Login/Login.jsx
@@ -9,36 +9,36 @@ function Login() {
   return (
     <div className='h-full w-full flex flex-col justify-start items-center gap-5 mt-10'>
         <div className='w-10/12'>
-            <h2 className='text-start text-2xl font-semibold'>LOGIN</h2>
+            <h2 className=' text-center lg:text-start text-2xl font-semibold'>LOGIN</h2>
          </div>
 
-        <div className='h-96 w-full flex justify-center items-center'>
+        <div className=' h-screen lg:h-96 w-full flex justify-center items-start md:items-center'>
 
-            <div className='h-full w-10/12 flex flex-col items-start justify-between'>
+            <div className='h-5/6 md:h-full w-10/12 flex flex-col items-start justify-start md:justify-between'>
             
 
-                <div className='flex h-full w-full justify-between items-center'>
+                <div className='flex flex-col lg:flex-row h-full w-full justify-between items-center'>
 
-                    <div className='h-full w-5/12 flex flex-col justify-evenly items-center'>
+                    <div className='h-full md:h-4/6 lg:h-full w-full md:w-10/12 lg:w-5/12 flex flex-col justify-evenly md:justify-center md:gap-5 lg:justify-evenly lg:gap-0 items-center'>
 
-                        <div className='w-full h-5/6 shadow-xl rounded-md flex justify-center items-center '>
+                        <div className='w-full h-5/6 md:h-3/6 lg:h-5/6 shadow-xl rounded-md flex justify-center items-center '>
 
                             <div className='w-full h-5/6 flex flex-col justify-around items-center px-5'>
 
-                                <input type='email' className='border w-full h-16 rounded-md pl-2 px-5 focus:outline-none focus:shadow-lg' placeholder='Your Email'/>
-                                <input type='password' className='border w-full h-16 rounded-md pl-2 px-5 focus:outline-none focus:shadow-lg' placeholder='Your Password' />
+                                <input type='email' className='border w-full h-12 md:h-16 rounded-md pl-2 px-5 focus:outline-none focus:shadow-lg' placeholder='Your Email'/>
+                                <input type='password' className='border w-full h-12 md:h-16 rounded-md pl-2 px-5 focus:outline-none focus:shadow-lg' placeholder='Your Password' />
 
                                 <div className='w-full flex justify-between items-center'>
                                     <input 
                                     type="button" 
                                     value="Login" 
-                                    className='py-3 px-5 hover:bg-cyan-200 focus:outline-none hover:scale-105 border border-cyan-400 bg-cyan-400 text-lg font-semibold rounded-md w-5/12 shadow-lg'
+                                    className='py-1 md:py-3 px-5 h-10 md:h-14 hover:bg-cyan-200 focus:outline-none hover:scale-105 border border-cyan-400 bg-cyan-400 text-lg font-semibold rounded-md w-5/12 shadow-lg'
                                     />
 
                                     <input 
                                     type="button" 
                                     value="Signup" 
-                                    className='py-3 px-5 focus:outline-none hover:scale-105 hover:bg-cyan-400 hover:text-white text-cyan-400 border border-cyan-400 bg-white text-lg font-semibold rounded-md w-5/12 '
+                                    className=' py-1 md:py-3 px-5 h-10 md:h-14 focus:outline-none hover:scale-105 hover:bg-cyan-400 hover:text-white text-cyan-400 border border-cyan-400 bg-white text-lg font-semibold rounded-md w-5/12 '
                                     />
                                 </div>
                         </div>
@@ -60,21 +60,21 @@ function Login() {
 
                                 </div>
 
-                                <div className='w-full flex items-center justify-between'>
-                                    <div className='w-5/12 flex justify-end'>
+                                <div className='w-full flex items-center justify-center gap-16'>
+                                    <button type='button' className='w-1/12 flex justify-end'>
                                         <FaFacebook className='text-cyan-400 text-2xl hover:text-cyan-200' />
-                                    </div>
+                                    </button>
 
-                                    <div className='w-5/12 flex justify-start'>
+                                    <button type='button' className='w-1/12 flex justify-start'>
                                         <GrGoogle className='text-cyan-400 text-2xl hover:text-cyan-200' />
-                                    </div>
+                                    </button>
                                 </div>
 
                         </div>
 
                     </div>
 
-                    <div className='w-6/12 h-5/6 flex items-start'>
+                    <div className='w-full md:w-10/12 lg:w-6/12 h-5/6 flex items-center md:items-start'>
                         <img src={loginPicture} alt='login_picture' />
                     </div>
                 </div>

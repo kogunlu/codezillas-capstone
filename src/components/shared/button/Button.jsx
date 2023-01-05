52ff348d096664f3f93fdd4f4c5837af2b3db0a3
admin <nour.majed1234@gmail.com>
2023-01-05 18:33:46 +0300

    update button & create test file

--- src/components/shared/button/Button.jsx
+++ src/components/shared/button/Button.jsx
@@ -1,10 +1,24 @@
 /* eslint-disable react/destructuring-assignment */
+import { useNavigate } from "react-router-dom";
+
 export default function Button(props)
 {
+    const navigate = useNavigate();
+
+    function handleClick()
+    {
+        if(props.function)
+        {
+            props.function()
+        }else{
+            navigate('/')
+        }
+    }
     return(
         <input 
         type={props.type}
         value={props.name}
-        className={props.classes} />
+        className={props.classes}
+        onClick={()=> handleClick()} />
     );
 }

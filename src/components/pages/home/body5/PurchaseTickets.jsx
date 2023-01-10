37b8cf7d3a6f04564ef893d642db818b99bbae0f
Kuday Ogunlu <kudayogunlu@gmail.com>
2023-01-10 19:39:50 +0300

    Fix | Responsiveness improve

--- src/components/pages/home/body5/PurchaseTickets.jsx
+++ src/components/pages/home/body5/PurchaseTickets.jsx
@@ -1,32 +1,40 @@
-import React from "react";
-import PurchaseCard from "./PurchaseCard";
+import React from 'react';
+import PurchaseCard from './PurchaseCard';
 
-function PurchaseTickets(){
-    return(
-<div>
-        <p className='text-4xl font-normal pt-14 pb-6 text-center lg:text-start lg:px-40 2xl:text-8xl 2xl:px-80'>PURCHASE TICKETS</p>
-        <p className='text-l font-normal pb-16 text-center lg:text-start lg:px-40 2xl:text-4xl 2xl:px-80'>PURCHASE TICKETS THAT CAN BE USED TO BOOK APPOINTMENTS!</p>
-
-    <div className="flex flex-col gap-4 2xl:gap-20">
-    <div className="flex flex-wrap justify-center w-full gap-4 2xl:justify-start 2xl:px-80 2xl:gap-40">  
-        <PurchaseCard className="w-40" title="5 TICKETS" dollar="10$"
-        button="PURCHASE"/>
-          
-        <PurchaseCard title="25 TICKETS" dollar="40$"
-        button="PURCHASE"/>
-         
-        <PurchaseCard title="50 TICKETS" dollar="70$"
-        button="PURCHASE"/>
-    </div>
-
+function PurchaseTickets() {
+  return (
     <div className="flex justify-center">
-        <PurchaseCard title="ARE YOU A COUNSELOR?" text="interested in joining our mental health platform? you decide your schedule and how much you want to work, we'll take care of the client referrals and billing details!"
-        button="LEARN MORE"/>
-    </div>
-    </div>
+      <div className="w-5/6 flex flex-col items-center">
+        <div className="w-5/6 flex flex-col justify-between items-start">
+          <p className="text-4xl mt-12 mb-6 text-center lg:text-start 2xl:text-6xl font-bold ">
+            PURCHASE TICKETS
+          </p>
+          <p className="text-l font-normal mb-16 text-center lg:text-start 2xl:text-3xl ">
+            PURCHASE TICKETS THAT CAN BE USED TO BOOK APPOINTMENTS!
+          </p>
+        </div>
 
-</div>
-    )
+        <div className="w-5/6 flex flex-col gap-4 2xl:gap-20">
+          <div className="flex flex-wrap justify-center lg:justify-between w-full gap-4 ">
+            <PurchaseCard title="5 TICKETS" dollar="10$" button="PURCHASE" />
+
+            <PurchaseCard title="25 TICKETS" dollar="40$" button="PURCHASE" />
+
+            <PurchaseCard title="50 TICKETS" dollar="70$" button="PURCHASE" />
+          </div>
+
+          <div className="flex justify-center mb-10">
+            <PurchaseCard
+              classList="border border-solid text-center w-full border-gray-200 rounded-lg shadow-xl"
+              title="ARE YOU A COUNSELOR?"
+              text="interested in joining our mental health platform? you decide your schedule and how much you want to work, we'll take care of the client referrals and billing details!"
+              button="LEARN MORE"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
-       
-export default PurchaseTickets
+
+export default PurchaseTickets;

a7987c2c241a8e83f5e3df8214f0bf2dcc6819a6
Sara Youssef <sarary2000@gmail.com>
2023-01-11 23:33:40 +0300

    Done ready for review

--- src/components/pages/home/Body3/Body3.jsx
+++ src/components/pages/home/Body3/Body3.jsx
@@ -5,8 +5,8 @@ const Body3 = () => {
   return (
     <div>
       <div className=' bg-white flex  justify-center  max-w-[ 1440px] min-w-[500px] '>
-    <div className='w-8/12 h-screen flex flex-col items-start  justify-evenly min-h-[1000px] '>
-    <div className='  w-3/4  text-5xl font-medium uppercase font-sans mt-20 mb-10'><h3>we can communicate through</h3></div>
+    <div className='w-8/12 h-screen flex flex-col items-start  justify-evenly min-h-[1400px] '>
+    <div className='  text-5xl font-medium uppercase font-sans mt-40 mb-10'><h3>we can communicate through</h3></div>
          
 <Cards /> 
 

5e06d43597b6e14188fa846bff2e37e7fb07b0cb
admin <nour.majed1234@gmail.com>
2023-01-17 16:41:27 +0300

    Body1 test updated

--- src/components/pages/home/__test__/Body1.test.js
+++ src/components/pages/home/__test__/Body1.test.js
@@ -2,7 +2,7 @@ import renderer from 'react-test-renderer';
 import { BrowserRouter } from 'react-router-dom';
 import Body1 from '../Body1';
 
-test('Navbar component renders correctly', () => {
+test('Body1 component renders correctly', () => {
   const tree = renderer.create(<BrowserRouter><Body1/></BrowserRouter>).toJSON();
   expect(tree).toMatchSnapshot();
 });

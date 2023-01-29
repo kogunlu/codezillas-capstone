30889ca1ed1ff591bec5f939b27f95700b6326ee
kudayOgunlu <kuday@techtile.com.tr>
2023-01-29 21:46:04 +0300

    fix test

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,20 +1,18 @@
-import React from 'react'
+import React from 'react';
 import PurchaseTickets from './body5/PurchaseTickets';
 import Slidernew from './body4/Slider';
-import Body2 from './Body2/Body2'
-import Body3 from './Body3/Body3'
-
+import Body2 from './Body2/Body2';
+import Body3 from './Body3/Body3';
 
 function Home() {
   return (
     <div>
-     <Slidernew/>
-     <PurchaseTickets/>
-     <Body2 />
+      <Body2 />
       <Body3 />
-
-      </div> 
+      <Slidernew />
+      <PurchaseTickets />
+    </div>
   );
 }
 
-export default Home 
+export default Home;
